8c2473ee00f034e7373f0a8bdd18cd368c4258da
pizzasperoni <diegosperoni1@gmail.com>
2016-10-05 16:39:46 -0300

    added some tasks + pages

--- firefoxPorn.sh
+++ firefoxPorn.sh
@@ -3,4 +3,7 @@ for i in {1..250}
 do
 	firefox http://31.media.tumblr.com/cb5c2127ac21fe60ff77b31939a59ba8/tumblr_muy6f42nY01sk816ro1_500.gif
 	nautilus
+	dolphin
+	software-center
+	firefox http://vignette2.wikia.nocookie.net/glee/images/8/83/Gay_kiss_hot_yum_sexy.gif/revision/latest?cb=20140323142924
 done
